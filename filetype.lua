e4212fa7e1f70c78371ae6e798eb64a5ada71be3
Jesse Hoyos <jesse@mojotech.com>
2025-01-06 19:59:38 -0500

    Add heex syntax awareness

--- filetype.lua
+++ filetype.lua
@@ -2,3 +2,9 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.astro",
     command = "set filetype=astro",
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.heex",
+    command = "set filetype=heex",
+})
+
