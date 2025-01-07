3880dbcca8416336ec53d388570966a1bda83a45
Jesse Hoyos <jesse@mojotech.com>
2025-01-06 19:59:17 -0500

    Format plugins

--- plugin/qg.lua
+++ plugin/qg.lua
@@ -1,12 +1,18 @@
 local Qgrep = function(args)
-    vim.api.nvim_exec_autocmds("QuickFixCmdPre", { pattern = "cgetexpr", modeline = false })
+    vim.api.nvim_exec_autocmds(
+        "QuickFixCmdPre",
+        { pattern = "cgetexpr", modeline = false }
+    )
 
     vim.fn.setqflist({}, " ", {
         title = "search results",
         lines = vim.fn.systemlist(vim.opt.grepprg:get() .. " " .. args),
     })
 
-    vim.api.nvim_exec_autocmds("QuickFixCmdPost", { pattern = "cgetexpr", modeline = false })
+    vim.api.nvim_exec_autocmds(
+        "QuickFixCmdPost",
+        { pattern = "cgetexpr", modeline = false }
+    )
 end
 
 vim.api.nvim_create_user_command("Qg", function(opts)
@@ -18,4 +24,9 @@ end, {
 })
 
 vim.keymap.set("n", "<leader>/", [[:Qg<space>]], { desc = "quick grep" })
-vim.keymap.set("n", "<leader>.", [[:Qg <C-R>=expand("<cword>")<CR><CR>]], { desc = "quick grep" })
+vim.keymap.set(
+    "n",
+    "<leader>.",
+    [[:Qg <C-R>=expand("<cword>")<CR><CR>]],
+    { desc = "quick grep" }
+)
