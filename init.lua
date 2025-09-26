675677b5237d8bdc8358a432dc7fca9366792848
Jesse Hoyos <jesse@mojotech.com>
2025-09-25 23:38:37 -0400

    Add yanky plugin

--- init.lua
+++ init.lua
@@ -482,3 +482,20 @@ later(function()
         },
     }
 end)
+
+later(function()
+    add "gbprod/yanky.nvim"
+    require("yanky").setup {}
+    vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+    vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+    vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+    vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+    vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+    vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
+    vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
+
+    vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+    vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+
+    vim.keymap.set({ "n", "x" }, "<leader>p", vim.cmd.YankyRingHistory)
+end)
